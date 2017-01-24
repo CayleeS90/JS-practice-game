3380306f3a975b2de3c5ae05f2796c499a744adc
Caylee <caylee.stewart@gmail.com>
2017-01-24 14:12:18 -0500

    added function and new alerts

--- scripts/jspractice.js
+++ scripts/jspractice.js
@@ -1,14 +1,17 @@
+
 var startStory = function () {
         "use strict";
         window.alert("The cool wet pitter patter of rain drops wakes you. You look around and and realize you are in the ruins of an old town. There's no sign of life...\nYou suddendly remember who you are and what your purpose is!");
-    
+    // random number function
         function randomNumber(i) {
             return Math.round(Math.random() * (i - 1));
         }
-    
+    // literal object
         var characterName =  window.prompt("You remember that your name is"),
             characterClassChoice = window.prompt("You are a:\n barbarian \n mage \n rogue hunter").toLowerCase(),
-            weapons = ["rock and bashed", "rusty pitchfork and stabbed", "shard of glass and slashed"],
+            weapons = ["rock and tried to bash", "rusty pitchfork and stabbed at", "shard of glass and slashed at"],
+            damage = ["You were not able to hit the monster!", "You were able to hit the monster once and flee!", "You hit the monster twice. It looks up at you and moans as the undead energy leaves its eyes.", "You hit the monster over and over again out of terror. what was once the animate corpse of a rogue woman is now a muddled mess of blood and mud."],
+            enemyHits = damage[randomNumber(damage.length)],
             randomWeapon = weapons[randomNumber(weapons.length)],
             character = {
                 health: 10,
@@ -19,7 +22,7 @@ var startStory = function () {
                 characterClass: characterClassChoice
             };
     
-   
+   // switch statement that corresponds with the object
         switch (characterClassChoice) {
         case "barbarian":
             window.alert("You are a barbarian. You have a strength bonus and you are more effective with melee weapons.");
@@ -35,10 +38,14 @@ var startStory = function () {
         }
         window.alert("You hear a noise behind you. You notice, what you thought was a corpse hanging from a post, moving and wiggling to loosen the chains that bind it. ");
         window.alert("You are startled and fall backwards. Without thinking you quickly feel around in the mud for something to use as a weapon. you found a " + randomWeapon + " the unnatural being!");
+    
+        window.alert(enemyHits);
+    
+        
+    
     };
 
-// testing 123
-
+// enter button function
 document.getElementById("beginButton").addEventListener("click", startStory, false);
 
 
